cc82e1ee374a92febfc02d3d0d3ac5136f4bdb4a
naman-1905 <namanoj19@gmail.com>
2025-11-26 18:31:06 +0530

    Replace main screen text with ApplicationForm component in LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ApplicationForm from '../components/ApplicationForm'
 
 function LandingPage() {
   return (
     <div>
-      Main Screen
+      <ApplicationForm/>
     </div>
   )
 }
