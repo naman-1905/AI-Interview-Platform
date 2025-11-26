b341efccba40c75537be7b89ee3d17911810f5a2
naman-1905 <namanoj19@gmail.com>
2025-11-26 17:42:20 +0530

    Add LandingPage component

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+      <h1 className='text-3xl font-bold'>Landing Page</h1>
+    </div>
+  )
+}
+
+export default Landing
