f673cd8337c92e7bf79fb7b4e6def764e0c82220
naman-1905 <namanoj19@gmail.com>
2025-11-27 00:11:40 +0530

    Implement InterviewPage with TimerBar and ChatWindow components for session management

--- frontend/src/pages/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import TimerBar from "../components/TimerBar";
+import TimeUpModal from "../components/TimeUpModal";
+import ChatWindow from "../components/ChatWindow";
 
 function InterviewPage() {
+  const [timeExpired, setTimeExpired] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>
-      Interview
+    <div className="min-h-screen bg-gray-50">
+      <div className="z-20 relative">
+        <TimerBar onTimeOver={() => setTimeExpired(true)} />
+      </div>
+
+      <ChatWindow className ="mt-5"
+        timeExpired={timeExpired}
+        onFinalResponse={() => setShowModal(true)}
+      />
+
+      {showModal && <TimeUpModal />}
     </div>
-  )
+  );
 }
 
-export default InterviewPage
+export default InterviewPage;
