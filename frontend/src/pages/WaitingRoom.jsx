00aa1e08bc417c9ef9149714952f5ee1119e91f9
naman-1905 <namanoj19@gmail.com>
2025-11-26 17:42:37 +0530

    Add WaitingRoom component

---
+++ frontend/src/pages/WaitingRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WaitingRoom() {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+      <h1 className='text-3xl font-bold'>Waiting Room</h1>
+    </div>
+  )
+}
+
+export default WaitingRoom
