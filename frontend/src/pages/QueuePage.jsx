519af5eebbe98dcea32941414c3fedf16cb5c8ae
naman-1905 <namanoj19@gmail.com>
2025-11-26 19:13:38 +0530

    Replace placeholder text with WaitingArea component in QueuePage

--- frontend/src/pages/QueuePage.jsx
+++ frontend/src/pages/QueuePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import WaitingArea from '../components/WaitingRoom'
 
 function QueuePage() {
   return (
     <div>
-      Queue
+      <WaitingArea/>
     </div>
   )
 }
