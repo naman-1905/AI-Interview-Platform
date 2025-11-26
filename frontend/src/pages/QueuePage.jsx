27b1e9a3c439e77b74b2e1ed7aabcafaa6e2d5b5
naman-1905 <namanoj19@gmail.com>
2025-11-27 00:11:53 +0530

    Add Navbar component to QueuePage for improved navigation

--- frontend/src/pages/QueuePage.jsx
+++ frontend/src/pages/QueuePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 import WaitingArea from '../components/WaitingRoom'
 
 function QueuePage() {
   return (
     <div>
+      <Navbar/>
       <WaitingArea/>
     </div>
   )
