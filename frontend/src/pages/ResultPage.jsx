74e2b99c08a4d0b17eb228905536690312680678
naman-1905 <namanoj19@gmail.com>
2025-11-27 15:38:01 +0530

    Remove the ability to go back in browser history to the Interview Page

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from '../components/Navbar'
 import SWOT from '../components/SWOT'
 
 function ResultPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // When user tries to go back, push them back here
+    window.history.pushState(null, "", window.location.href);
+
+    const preventBack = () => {
+      navigate("/result", { replace: true });
+    };
+
+    window.addEventListener("popstate", preventBack);
+
+    return () => {
+      window.removeEventListener("popstate", preventBack);
+    };
+  }, [navigate]);
+
   return (
     <div>
       <Navbar/>
       <SWOT/>
     </div>
-  )
+  );
 }
 
-export default ResultPage
+export default ResultPage;
